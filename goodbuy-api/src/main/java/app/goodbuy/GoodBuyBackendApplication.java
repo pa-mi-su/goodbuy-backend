dc068dac3ff9354b494ccae0528e110a0b3135e0
pa-mi-su <pamisu@protonmail.com>
2025-10-28 12:53:54 -0400

    refactor(api): place Spring Boot app and props inside goodbuy-api module

---
+++ goodbuy-api/src/main/java/app/goodbuy/GoodBuyBackendApplication.java
@@ -0,0 +1,11 @@
+package app.goodbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBuyBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoodBuyBackendApplication.class, args);
+    }
+}
