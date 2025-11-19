2ea18bae046fb01e7245aac45a819c90f94c26e1
pa-mi-su <pamisu@protonmail.com>
2025-11-19 11:26:24 -0500

    removed old migrations

--- goodbuy-adapters-core/src/main/resources/db/migration/V4__create_product_cache.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS product_cache (
-    gtin            VARCHAR(32) PRIMARY KEY,
-    json_payload    TEXT NOT NULL,
-    source          VARCHAR(64),
-    stored_at       TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
